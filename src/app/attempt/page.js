f4d5e92ec8f265a812fc74580983716824089787
Vegito2367 <tejaaditya.g@gmail.com>
2025-01-18 16:22:35 -0600

    Made a timer component

--- src/app/attempt/page.js
+++ src/app/attempt/page.js
@@ -2,42 +2,73 @@
 import { useEffect, useState } from "react"
 import validateLogin from "../middle";
 import Timer from "@/customComponents/timer";
-export default function Attempt(){
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export default function Attempt() {
 
     const [loggedIn, setLoggedIn] = useState(false);
     const [data, setData] = useState({});
-    useEffect(()=>{
+    const [showDialog, setShowDialog] = useState(false);
+    useEffect(() => {
         async function checkUser() {
-            try{
+            try {
                 const { status, response } = await validateLogin();
                 setData({ status: status, response: response });
-                if(status === 200){
+                if (status === 200) {
                     setLoggedIn(true);
                 }
             }
-            catch(err){
+            catch (err) {
                 console.log(err)
             }
-            
+
         }
         checkUser();
-    },[])
+    }, [])
 
-    if(!loggedIn){
+    function handleDialog() {
+        setShowDialog(!showDialog);
+    }
+
+    if (!loggedIn) {
         return (
             <section className="h-screen w-screen bg-gray-950 flex flex-col justify-center items-center text-white">
                 <h1>User not logged in</h1>
             </section>
         )
     }
-    else{
+    else {
         return (
             <section className="h-screen w-screen bg-gray-950 text-white">
                 <h1>Attempt page</h1>
                 <p>user logged in is : {data.response.user.email}</p>
-                <Timer totalTime={10} />
+                <Timer totalTime={10} callback={handleDialog} />
+
+
+                <AlertDialog open={showDialog} onOpenChange={handleDialog}>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Time has elapsed.</AlertDialogTitle>
+                            <AlertDialogDescription>Good job on the attempt!</AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Close</AlertDialogCancel>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+
             </section>
         )
     }
-    
+
 }
--- src/customComponents/timer.js
+++ src/customComponents/timer.js
@@ -1,22 +1,45 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
-export default function Timer({ totalTime }) {
+export default function Timer({ totalTime, callback }) {
 
     const [timeLeft, setTimeLeft] = useState(totalTime);
+    const [active,setActive]=useState(false);
+    const timeReference=useRef(null);
 
-    function startTimer() {
-        const eventID = setInterval(() => {
-            setTimeLeft(timeLeft - 1)
-        }, 1000)
 
-        setTimeout(() => {
-            clearInterval(eventID)
-        }, totalTime * 1000)
+    useEffect(()=>{
+        if(active){
+            timeReference.current=setInterval(()=>{
+                setTimeLeft((timeLeft)=>{
+                    if(timeLeft<=0){
+                        clearInterval(timeReference.current);
+                        setActive(false);
+                        timerFinished();
+                        return 0;
+                    }
+                    return timeLeft-1;
+                })
+            },1000)
+        }
+        else{
+            clearInterval(timeReference.current);
+        }
+        return ()=>{
+            clearInterval(timeReference.current);
+        }
+    },[active])
+
+    
+
+    function timerFinished(){
+        if(callback){
+            callback();
+        }
     }
 
-
-
-
+    function startTimer(){
+        setActive(true);
+    }
 
     return (
         <div>
