b5e0ff0ef5c2868ca88baeeb7c9f306f40ecfeb9
Vegito2367 <tejaaditya.g@gmail.com>
2025-01-28 19:09:20 -0600

    Updated title

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <section className="h-screen bg-gray-950 flex flex-col justify-center items-center">
         <p className="text-5xl text-white font-bold text-center py-10 animate-flyIn">
-          Hello welcome to the tabels practice module.
+          Hello, Welcome to MathQuest!
         </p>
         <div className="flex flex-row space-x-4">
           <a href="/signup" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
